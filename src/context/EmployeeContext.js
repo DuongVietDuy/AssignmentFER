d0221854c3ee7151cdc4a2a92849a726c6bdd32b
DucDung123456 <158832456+DucDung123456@users.noreply.github.com>
2024-07-11 13:02:58 +0700

    Update EmployeeContext.js

--- src/context/EmployeeContext.js
+++ src/context/EmployeeContext.js
@@ -9,6 +9,9 @@ const EmployeeProvider = ({ children }) => {
   const [employees, setEmployees] = useState([]);
   const [selectedDepartment, setSelectedDepartment] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +19,7 @@ const EmployeeProvider = ({ children }) => {
       setDepartments(deptResponse.data);
       const empResponse = await axios.get('http://localhost:9999/products');
       setEmployees(empResponse.data);
+      fetchLatestProducts(empResponse.data);
     };
     fetchData();
   }, []);
@@ -29,12 +33,65 @@ const EmployeeProvider = ({ children }) => {
     setEmployees([...employees, response.data]);
   };
 
+  const fetchLatestProducts = (products) => {
+    const sortedProducts = products.sort((a, b) => b.id - a.id);
+    const latestProducts = sortedProducts.slice(0, 4);
+    setLatestProducts(latestProducts);
+  };
+
   const getDepartmentName = (departmentId) => {
     const department = departments.find(dept => dept.id === departmentId);
     return department ? department.name : 'Unknown';
   };
+
+  const addToCart = (product, quantity) => {
+    const existingCart = cart.find(product1 => product1.id == product.id);
+    if(existingCart) {
+      setCart(cart.map(product1 =>
+        product1.id == product.id ? {...product1, quantity: product1.quantity + quantity} : product1
+      ));
+    }else{
+      setCart([...cart, { ...product, quantity}])
+    }
+  }
+
+  const increaseQuantity = (id) => {
+    setCart(cart.map(product1 =>
+      product1.id == id ? { ...product1, quantity: product1.quantity + 1 } : product1 
+    ));
+  }
+  const decreaseQuantity = (id) => {
+    setCart(cart.map(product1 => {
+      if (product1.id == id) {
+        if (product1.quantity > 1) {
+          return { ...product1, quantity: product1.quantity - 1 };
+        } else {
+          // return null; // Đánh dấu để xóa
+        }
+      }
+      return product1;
+    }).filter(product1 => product1 !== null));
+  };
+
+  const removeEmployeeFromTeam = (id) => {
+    setCart(cart.filter(product1 => product1.id != id));
+  };
+
+  const updateQuantity = (id, quantity) => {
+    setCart(cart.map(product1 => 
+      product1.id == id ? { ...product1, quantity } : product1
+    ));
+  };
+  
   return (
-    <EmployeeContext.Provider value={{ departments, employees, setEmployees, selectedDepartment, setSelectedDepartment, searchTerm, setSearchTerm, addEmployee, getDepartmentName }}>
+    <EmployeeContext.Provider value={{ departments, employees, setEmployees, selectedDepartment, setSelectedDepartment, searchTerm, setSearchTerm, addEmployee, getDepartmentName,
+      latestProducts,
+      cart,
+      addToCart,
+      increaseQuantity,
+      decreaseQuantity,
+      removeEmployeeFromTeam,
+      updateQuantity}}>
       {children}
     </EmployeeContext.Provider>
   );
