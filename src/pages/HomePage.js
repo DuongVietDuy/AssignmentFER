ba71d6899aa9a813bee4ee1a3bf8a52ff28d1ac9
DucDung123456 <158832456+DucDung123456@users.noreply.github.com>
2024-07-11 13:14:29 +0700

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import NavBars from '../components/NavBars';
+import AppCarousel from './Carousel'
+import NewProductList from './NewProductList'
 
 const HomePage = () => {
   return (
     <NavBars />
+    <>
+      <AppCarousel />      
+        <NewProductList />
+    </>
   )
 }
 
