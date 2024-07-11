9504f2368768cc2867900b68f933d52682c1fa5a
DucDung123456 <158832456+DucDung123456@users.noreply.github.com>
2024-07-11 13:17:27 +0700

    Update EmployeeDetail.js

--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -1,25 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Col, Row, Form } from 'react-bootstrap';
 import { EmployeeContext } from '../context/EmployeeContext';
 
 const EmployeeDetail = () => {
-  const { employees } = useContext(EmployeeContext);
+  const { employees, cart, addToCart } = useContext(EmployeeContext);
   const { id } = useParams();
   const navigate = useNavigate();
   const employee = employees.find(emp => emp.id == parseInt(id));
+
+  const handleAdd = (product1) => {
+    const quantity = 1;
+    addToCart(product1, quantity);
+  };
 
   if (!employee) {
     return <h2>Product not found</h2>;
   }
 
-  // Định nghĩa các kiểu nội tuyến cho ảnh
   const imageStyles = {
-    width: '300px', // Điều chỉnh kích thước rộng của ảnh
-    height: 'auto', // Giữ tỷ lệ của ảnh
-    objectFit: 'cover', // Đảm bảo ảnh luôn phù hợp trong khung
+    width: '300px', 
+    height: 'auto', 
+    objectFit: 'cover',
     display: 'block',
-    margin: '0 auto' // Căn giữa ảnh trong thẻ
+    margin: '0 auto' 
   };
 
   return (
@@ -27,24 +31,31 @@ const EmployeeDetail = () => {
       <Button variant="secondary" onClick={() => navigate('/shop')}>
         Back to List
       </Button>
-      <Card>
-        <Card.Img
-          variant="top"
-          src={employee.image || 'default-image-url'}
-          alt="Employee"
-          style={imageStyles} // Sử dụng kiểu nội tuyến
-        />
-        <Card.Body>
-          <Card.Title>{employee.name}</Card.Title>
-          <Card.Text>
-            <strong>Price:</strong> {employee.price}<br/>
-            <strong>DeviceID:</strong> {employee.deviceID}<br/>
-            <strong>Brand:</strong> {employee.brand}<br/>
-            <strong>Model:</strong> {employee.model}<br/>
-            <strong>Specifications:</strong> {JSON.stringify(employee.specifications)}<br/>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      
+        <Card><Row>
+          <Col md={8}>
+            <Card.Img
+              variant="top"
+              src={employee.image || 'default-image-url'}
+              alt="Employee"
+              style={imageStyles} 
+            />
+          </Col>
+          <Col md={4}>
+            <Card.Body>
+              <Card.Title>{employee.name}</Card.Title>
+              <Card.Text>
+                <strong>Price:</strong> ${employee.price}<br />
+                <strong>DeviceID:</strong> {employee.deviceID}<br />
+                <strong>Brand:</strong> {employee.brand}<br />
+                <strong>Model:</strong> {employee.model}<br />
+                <strong>Specifications:</strong> {JSON.stringify(employee.specifications)}<br />
+              </Card.Text>
+              <Button variant='primary' onClick={() => handleAdd(employee)}>Add to Cart</Button>
+            </Card.Body>
+          </Col></Row>
+        </Card>
+      
     </div>
   );
 };
